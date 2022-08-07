3ab8d8c8e674a1cc591c9213a0d317f78a5e0fc4
AdminS <qvipixer@gmail.com>
2022-08-08 00:50:52 +0300

    start

--- config.py
+++ config.py
@@ -2,18 +2,18 @@ from aiogram.dispatcher import Dispatcher
 from aiogram import Bot
 import os
 
-TOKEN = os.getenv('5466053732:AAHZJK3wOusCldo3xTNAkevzEKAVGVqcqXM')
+TOKEN = os.getenv("5466053732:AAHZJK3wOusCldo3xTNAkevzEKAVGVqcqXM")
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-HEROKU_APP_NAME = os.getenv('desolate-caverns-75711')
+HEROKU_APP_NAME = os.getenv("desolate-caverns-75711")
 
 # webhook settings
-WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
-WEBHOOK_PATH = f'/webhook/{TOKEN}'
-WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+WEBHOOK_HOST = f"https://{HEROKU_APP_NAME}.herokuapp.com"
+WEBHOOK_PATH = f"/webhook/{TOKEN}"
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
 # webserver settings
-WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = os.getenv('PORT', default=8000)
-DB_URL = os.getenv('DATABASE_URL')
+WEBAPP_HOST = "0.0.0.0"
+WEBAPP_PORT = os.getenv("PORT", default=8000)
+DB_URL = os.getenv("DATABASE_URL")
--- main.py
+++ main.py
@@ -16,15 +16,17 @@ async def on_shutdown(dispatcher):
 
 
 async def save(user_id, text):
-    await database.execute(f"INSERT INTO messages(telegram_id, text) "
-                           f"VALUES (:telegram_id, :text)", values={'telegram_id': user_id, 'text': text})
+    await database.execute(
+        f"INSERT INTO messages(telegram_id, text) " f"VALUES (:telegram_id, :text)",
+        values={"telegram_id": user_id, "text": text},
+    )
 
 
 async def read(user_id):
-    results = await database.fetch_all('SELECT text '
-                                       'FROM messages '
-                                       'WHERE telegram_id = :telegram_id ',
-                                       values={'telegram_id': user_id})
+    results = await database.fetch_all(
+        "SELECT text " "FROM messages " "WHERE telegram_id = :telegram_id ",
+        values={"telegram_id": user_id},
+    )
     return [next(result.values()) for result in results]
 
 
@@ -35,7 +37,7 @@ async def echo(message: types.Message):
     await message.answer(messages)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     start_webhook(
         dispatcher=dp,
