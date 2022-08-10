da298b11d3aff6041ed227867d77b215f1153c2a
AdminS <qvipixer@gmail.com>
2022-08-10 12:13:15 +0300

    up test

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import logging
-from aiogram import types
+from aiogram import Bot, Dispatcher, executor, types
 from aiogram.utils.executor import start_webhook
+from aiogram.utils.callback_data import CallbackData
+import aiogram.utils.markdown as fmt
 from config import bot, dp, WEBHOOK_URL, WEBHOOK_PATH, WEBAPP_HOST, WEBAPP_PORT
+import mods
+from random import randint
 
 
 async def on_startup(dispatcher):
@@ -22,6 +26,8 @@ async def echo(message: types.Message):
     await message.answer(messages)
 """
 
+""" MENU"""
+
 
 @dp.message_handler(commands=["start"])
 async def menu_start_command(message: types.Message):
@@ -31,6 +37,107 @@ async def menu_start_command(message: types.Message):
 
     await message.answer("Добро пожаловать!", reply_markup=menu_kb)
 
+
+@dp.message_handler(lambda message: message.text == "Вызвать меню")
+async def without_puree(message: types.Message):
+    menu_kb_inl = types.InlineKeyboardMarkup(resize_keyboard=True)
+
+    button_inl_weather = types.KeyboardButton(
+        text="Погода", callback_data="weather_value"
+    )
+    button_inl_projects = types.KeyboardButton(
+        text="Проекты", callback_data="projects_value"
+    )
+    button_inl_humor = types.KeyboardButton(text="Шутки", callback_data="humor_value")
+    button_inl_random = types.KeyboardButton(
+        text="Рандом", callback_data="random_value"
+    )
+    button_inl_nasa_apod_photo = types.KeyboardButton(
+        text="NASA APOD", callback_data="nasa_apod_photo"
+    )
+    button_inl_nasa_epic_photo = types.KeyboardButton(
+        text="NASA EPIC", callback_data="nasa_epic_photo"
+    )
+
+    button_inl_site = types.KeyboardButton(
+        text="Наш сайт", url="http://craft-projects.com"
+    )
+
+    menu_kb_inl.add(button_inl_weather, button_inl_projects)
+    menu_kb_inl.add(button_inl_humor, button_inl_random)
+    menu_kb_inl.add(button_inl_nasa_apod_photo, button_inl_nasa_epic_photo)
+    menu_kb_inl.add(button_inl_site)
+
+    await message.reply("Добро пожаловать в меню!", reply_markup=menu_kb_inl)
+
+
+@dp.callback_query_handler(text="random_value")
+async def send_random_value(call: types.CallbackQuery):
+    menu_kb_inl = types.InlineKeyboardMarkup(resize_keyboard=True)
+    button_inl_random = types.KeyboardButton(
+        text="Сыграем?", callback_data="random_value"
+    )
+    menu_kb_inl.add(button_inl_random)
+
+    await call.message.answer(
+        ("Число от 0 до 10 было :  " + str(randint(0, 10))), reply_markup=menu_kb_inl
+    )
+
+
+@dp.callback_query_handler(text="weather_value")
+async def send_weather_value(call: types.CallbackQuery):
+    weather_list = mods.weather()
+
+    await call.message.answer(
+        fmt.text(
+            fmt.text("Погода в Жлобине"),
+            fmt.text("Вчера в это время: " + weather_list[0]),
+            fmt.text("Сейчас: " + weather_list[1]),
+            fmt.text("Ощущается как : " + weather_list[2]),
+            fmt.text("Ветер: " + weather_list[3]),
+            fmt.text("Влажность: " + weather_list[4]),
+            fmt.text("Давление: " + weather_list[5]),
+            fmt.text("Световой день: " + weather_list[6]),
+            fmt.text(weather_list[7]),
+            fmt.text(weather_list[8]),
+            fmt.text(weather_list[9]),
+            sep="\n",
+        ),
+        parse_mode="HTML",
+    )
+    # await call.message.reply('Тут скоро будут погодные данные')
+
+
+@dp.callback_query_handler(text="nasa_apod_photo")
+async def send_nasa_apod_photo(call: types.CallbackQuery):
+    nasa_list = mods.nasa_apod()
+
+    await call.message.answer_photo(nasa_list[0], caption=nasa_list[1])
+
+
+@dp.callback_query_handler(text="nasa_epic_photo")
+async def send_nasa_epic_photo(call: types.CallbackQuery):
+    nasa_list = mods.nasa_epic()
+    await call.message.answer_photo(nasa_list[0], caption=nasa_list[1])
+
+
+@dp.callback_query_handler(text="projects_value")
+async def send_projects_value(call: types.CallbackQuery):
+    await call.message.reply("Тут скоро будут данные о проектах")
+
+
+@dp.callback_query_handler(text="humor_value")
+async def send_humor_value(call: types.CallbackQuery):
+    menu_kb_inl = types.InlineKeyboardMarkup(resize_keyboard=True)
+    button_inl_random = types.KeyboardButton(text="Ещё", callback_data="humor_value")
+    menu_kb_inl.add(button_inl_random)
+
+    await call.message.answer(
+        mods.humor(), reply_markup=menu_kb_inl
+    )  # +'Тут скоро будут шутки-прибаутки')
+
+
+"""MENU"""
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
---
+++ mods.py
@@ -0,0 +1,129 @@
+import json
+import requests
+import xmltodict
+import urllib.request
+from bs4 import BeautifulSoup
+
+
+def humor():
+    """
+    1 - Анекдот;
+    2 - Рассказы;
+    3 - Стишки;
+    4 - Афоризмы;
+    5 - Цитаты;
+    6 - Тосты;
+    8 - Статусы;
+    11 - Анекдот (+18);
+    12 - Рассказы (+18);
+    13 - Стишки (+18);
+    14 - Афоризмы (+18);
+    15 - Цитаты (+18);
+    16 - Тосты (+18);
+    18 - Статусы (+18);
+    """
+
+    response = requests.get("http://rzhunemogu.ru/Rand.aspx?CType=2")
+    humor_text_xml = xmltodict.parse(response.text)
+    humor_text_output = humor_text_xml["root"]["content"]
+    return str(humor_text_output)
+
+
+"""""" """""" """""" """""" """""" """""" """''
+
+""" """""" """""" """""" """""" """""" """""" ""
+
+
+def nasa_apod():
+    response = requests.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+    nasa_photo_url = json.loads(response.text)["url"]
+    nasa_photo_title = json.loads(response.text)["title"]
+    image = [nasa_photo_url, nasa_photo_title]
+    return image
+
+
+def nasa_epic():
+    response = requests.get("https://epic.gsfc.nasa.gov/api/images.php")
+    nasa_photo_url = json.loads(response.text)[-1]["image"]
+    nasa_photo_cap = json.loads(response.text)[-1]["caption"]
+    image = [
+        "https://epic.gsfc.nasa.gov/epic-archive/jpg/" + nasa_photo_url + ".jpg",
+        nasa_photo_cap,
+    ]
+    return image
+
+
+def weather():
+    """
+    https://www.cyberforum.ru/python-web/thread1884117.html
+    """
+
+    url = "https://yandex.by/pogoda/zhlobin"
+    response = urllib.request.urlopen(url)
+    html = response.read()
+    soup = BeautifulSoup(html, "html.parser")
+    yesterday_temp_val = (
+        soup.find("div", class_="fact__time-yesterday-wrap")
+        .find("span", class_="temp__value " "temp__value_with-unit" "")
+        .get_text()
+    )
+    # print('Вчера в это время: ' + yesterday_temp_val)
+    now_temp_val = (
+        soup.find("div", class_="temp fact__temp fact__temp_size_s")
+        .find("span", class_="temp__value " "temp__value_with-unit" "")
+        .get_text()
+    )
+    # print('Сейчас: ' + now_temp_val)
+    feelings_temp_val = (
+        soup.find("div", class_="link__feelings fact__feelings")
+        .find("span", class_="temp__value " "temp__value_with-unit" "")
+        .get_text()
+    )
+    # print('Ощущается как : ' + feelings_temp_val)
+    wind_speed_val = soup.find(
+        "div", class_="term term_orient_v fact__wind-speed"
+    ).get_text()
+    # print('Ветер: ' + wind_speed_val)
+    humidity_val = soup.find(
+        "div", class_="term term_orient_v fact__humidity"
+    ).get_text()
+    # print('Влажность: ' + humidity_val)
+    pressure_val = soup.find(
+        "div", class_="term term_orient_v fact__pressure"
+    ).get_text()
+    # print('Давление: ' + pressure_val)
+    day_duration_val = soup.find(
+        "div", class_="sun-card__day-duration-value"
+    ).get_text()
+    # print('Световой день: ' + day_duration_val)
+    sun_rise_val = soup.find(
+        "div",
+        class_="sun-card__sunrise-sunset-info sun-card__sunrise-sunset-info_value_rise-time",
+    ).get_text()
+    # print('Восход: ' + sun_rise_val)
+    sun_set_val = soup.find(
+        "div",
+        class_="sun-card__sunrise-sunset-info sun-card__sunrise-sunset-info_value_set-time",
+    ).get_text()
+    # print('Закат: ' + sun_set_val)
+    text_info_val = soup.find("div", class_="sun-card__text-info").get_text()
+    # print('Инфо: ' + text_info_val)
+    weather_val = [
+        yesterday_temp_val,
+        now_temp_val,
+        feelings_temp_val,
+        wind_speed_val,
+        humidity_val,
+        pressure_val,
+        day_duration_val,
+        sun_rise_val,
+        sun_set_val,
+        text_info_val,
+    ]
+    return weather_val
+
+
+# print(weather())
+# print(humor())
+# print(nasa_apod())
+# print(nasa_epic())
