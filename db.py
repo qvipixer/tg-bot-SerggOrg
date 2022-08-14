209b60a18981399d4af1d4dcc3e95733b8b1b687
AdminS <qvipixer@gmail.com>
2022-08-14 11:49:25 +0300

    up

--- db.py
+++ db.py
@@ -1,7 +1,15 @@
+"""
 from databases import Database
 
 from config import DB_URL
 
 database = Database(DB_URL)
 
-# print()
+"""
+import os
+import psycopg2
+
+database = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+cursor = conn.cursor()
